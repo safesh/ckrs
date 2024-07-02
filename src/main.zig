4e21226feaa02d9519c6da276399bdedf03d5050
Mulling <lmulling@proton.me>
2024-07-02 00:26:52 -0300

    Add PKCS #11 (v2.40 and v3.00) headers

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,13 @@
 const std = @import("std");
 
+const pkcs11 = @cImport({
+    @cDefine("CK_PTR", "*");
+    @cDefine("CK_DECLARE_FUNCTION(returnType, name)", "returnType name");
+    @cDefine("CK_DECLARE_FUNCTION_POINTER(returnType, name)", "returnType (* name)");
+    @cDefine("CK_CALLBACK_FUNCTION(returnType, name)", "returnType (* name)");
+    @cInclude("v300/include/pkcs11.h");
+});
+
 pub fn main() !void {
     std.debug.print("Hello, World!\n", .{});
 }
